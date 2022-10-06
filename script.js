02c87924f551ee3dacc371a4a715a2a6c88e1378
Wendy <50072566+vendaciki@users.noreply.github.com>
2022-10-06 18:28:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+﻿// Získání tlačítko
+let mybutton = document.getElementById("myBtn");
+
+// Když se uživatel posune dolů o 800 pixelů od horní části dokumentu, zobrazte tlačítko
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 800) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// Když uživatel klikne na tlačítko, přejde na začátek dokumentu
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
